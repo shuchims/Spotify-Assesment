dd334bd5dd89a8d84c8cd11806016e76ab992ef5
shuchi4 <shuchimehra639@gmail.com>
2024-12-07 11:43:04 +0530

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Shuchi Mehra Sharma on 07/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpotifyTests/SpotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyTests.swift
+//  SpotifyTests
+//
+//  Created by Shuchi Mehra Sharma on 07/12/24.
+//
+
+import Testing
+@testable import Spotify
+
+struct SpotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
